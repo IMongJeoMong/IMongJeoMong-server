b0ede120ca31431d3a48aa46cf9230e20cd7dbbc
Junyoung <wnsdud12365@naver.com>
2023-06-25 21:33:48 +0900

    [IMJM-14] feat: Custom Exception, Custom Response 틀 생성

---
+++ src/main/java/com/imongjeomong/imongjeomongserver/exception/SampleException.java
@@ -0,0 +1,8 @@
+package com.imongjeomong.imongjeomongserver.exception;
+
+public class SampleException extends CommonException{
+
+    public SampleException(CustomExceptionStatus customExceptionStatus) {
+        super(customExceptionStatus);
+    }
+}
---
+++ src/main/java/com/imongjeomong/imongjeomongserver/response/DataResponse.java
@@ -0,0 +1,14 @@
+package com.imongjeomong.imongjeomongserver.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DataResponse<T> extends CommonResponse {
+
+    private T data;
+    
+}
