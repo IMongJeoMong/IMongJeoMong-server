9a96bcf6b82a97d82faeb67ab184a1b4804c5d63
Junyoung <wnsdud12365@naver.com>
2023-06-26 22:40:12 +0900

    feat: 회원 엔티티 생성

---
+++ src/main/java/com/imongjeomong/imongjeomongserver/member/controller/MemberController.java
@@ -0,0 +1,8 @@
+package com.imongjeomong.imongjeomongserver.member.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+}
