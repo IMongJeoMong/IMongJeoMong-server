74eb93e0bed53389c5ad926146b948075991d181
ChanHwan <song960705@naver.com>
2023-06-27 16:15:22 +0900

    feat: 주차장 공공데이터 DB화 구현

--- src/main/java/com/imongjeomong/imongjeomongserver/opendata/parking/ParkingLotRepository.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/opendata/parking/ParkingLotRepository.java
@@ -1,2 +1,8 @@
-package com.imongjeomong.imongjeomongserver.opendata.parking;public class ParkingLotRepository {
+package com.imongjeomong.imongjeomongserver.opendata.parking;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
 }
