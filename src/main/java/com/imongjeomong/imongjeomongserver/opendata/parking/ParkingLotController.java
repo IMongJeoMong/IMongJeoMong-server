efc71ec0e354ffa0b9a5380fe33d3cd2e9237974
ChanHwan <song960705@naver.com>
2023-06-27 16:15:22 +0900

    feat: 주차장 공공데이터 DB화 구현

--- src/main/java/com/imongjeomong/imongjeomongserver/opendata/parking/ParkingLotController.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/opendata/parking/ParkingLotController.java
@@ -1,17 +1,21 @@
 package com.imongjeomong.imongjeomongserver.opendata.parking;
 
 import com.imongjeomong.imongjeomongserver.opendata.tasu.TasuService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
 
 @RestController
+@RequiredArgsConstructor
 public class ParkingLotController {
 
-    @GetMapping("/opendata/parking/save")
-    public void saveParkingLot() throws UnsupportedEncodingException {
+    private final ParkingLotService parkingLotService;
 
+    @GetMapping("/opendata/parking-lot/save")
+    public void saveParkingLot() throws UnsupportedEncodingException {
+        parkingLotService.saveParkingLotInfo();
     }
 
 }
