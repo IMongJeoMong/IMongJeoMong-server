b256861f450ea830cf2c7520359684cdcd42110c
cksghks89 <song960705@naver.com>
2023-06-28 16:31:59 +0900

    refactor: entasu, parkinglog 엔티티 경로 수정

--- src/main/java/com/imongjeomong/imongjeomongserver/opendata/parking/ParkingLotRepository.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/opendata/parking/ParkingLotRepository.java
@@ -1,5 +1,6 @@
 package com.imongjeomong.imongjeomongserver.opendata.parking;
 
+import com.imongjeomong.imongjeomongserver.entity.ParkingLot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
