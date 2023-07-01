a9fbcb31a18e6a28cb1a420b1d8b19f4b0907688
Junyoung <wnsdud12365@naver.com>
2023-07-01 11:38:18 +0900

    feat: 관광지 조회(리스트, 단일) 구현 완료

--- src/main/java/com/imongjeomong/imongjeomongserver/attraction/model/service/AttractionService.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/attraction/model/service/AttractionService.java
@@ -1,5 +1,6 @@
 package com.imongjeomong.imongjeomongserver.attraction.model.service;
 
+import com.imongjeomong.imongjeomongserver.dto.AttractionDTO;
 import com.imongjeomong.imongjeomongserver.entity.Attraction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,5 +9,7 @@ import java.util.Map;
 
 public interface AttractionService {
 
-    Page<Attraction> getAttractionList(Map<String, Object> paramMap, Pageable pageable);
+    Page<AttractionDTO> getAttractionList(Map<String, Object> paramMap, Pageable pageable);
+
+    AttractionDTO getAttractionInfo(Long attractionId);
 }
