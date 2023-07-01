1798e77c0d92d8c737afa535aae5ea731923be74
Junyoung <wnsdud12365@naver.com>
2023-07-01 22:53:27 +0900

    feat: 관광지 방문 처리

--- src/main/java/com/imongjeomong/imongjeomongserver/attraction/model/repository/MyAttractionRepository.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/attraction/model/repository/MyAttractionRepository.java
@@ -1,9 +1,18 @@
 package com.imongjeomong.imongjeomongserver.attraction.model.repository;
 
+import com.imongjeomong.imongjeomongserver.dto.MyAttractionDTO;
 import com.imongjeomong.imongjeomongserver.entity.MyAttraction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MyAttractionRepository extends JpaRepository<MyAttraction, Long> {
+
+    Page<MyAttractionDTO> findAllByMemberId(Long memberId, Pageable pageable);
+
+    Optional<MyAttraction> findByMemberIdAndAttractionId(Long attractionId, Long memberId);
 }
