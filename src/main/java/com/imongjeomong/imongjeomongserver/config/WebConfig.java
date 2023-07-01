192fd73daac404359ab6b37607e8cd6084e2eefb
ChanHwan <song960705@naver.com>
2023-07-01 22:42:54 +0900

    feat: cors 설정

--- src/main/java/com/imongjeomong/imongjeomongserver/config/WebConfig.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/config/WebConfig.java
@@ -7,6 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    /**
+     * 전역으로 CORS 설정
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         WebMvcConfigurer.super.addCorsMappings(registry);
