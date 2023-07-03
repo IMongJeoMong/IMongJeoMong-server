ea87960c40dc59f08da7c318e9158fa43a10715c
Junyoung <wnsdud12365@naver.com>
2023-07-03 15:41:28 +0900

    feat: 일일 미션(단일) 클리어 구현

--- src/main/java/com/imongjeomong/imongjeomongserver/quest/model/service/QuestService.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/quest/model/service/QuestService.java
@@ -1,10 +1,13 @@
 package com.imongjeomong.imongjeomongserver.quest.model.service;
 
 import com.imongjeomong.imongjeomongserver.dto.MyQuestDTO;
+import com.imongjeomong.imongjeomongserver.entity.Member;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 public interface QuestService {
     List<MyQuestDTO> getDailyQuestList(HttpServletRequest request);
+
+    Member getQuestReward(HttpServletRequest request, Long myQuestId);
 }
--- src/main/java/com/imongjeomong/imongjeomongserver/quest/model/service/QuestServiceImpl.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/quest/model/service/QuestServiceImpl.java
@@ -1,9 +1,11 @@
 package com.imongjeomong.imongjeomongserver.quest.model.service;
 
 import com.imongjeomong.imongjeomongserver.dto.MyQuestDTO;
+import com.imongjeomong.imongjeomongserver.entity.Member;
 import com.imongjeomong.imongjeomongserver.entity.MyQuest;
 import com.imongjeomong.imongjeomongserver.exception.CommonException;
 import com.imongjeomong.imongjeomongserver.exception.CustomExceptionStatus;
+import com.imongjeomong.imongjeomongserver.member.model.repository.MemberRepository;
 import com.imongjeomong.imongjeomongserver.quest.model.repository.MyQuestRepository;
 import com.imongjeomong.imongjeomongserver.quest.model.repository.QuestRepositoy;
 import com.imongjeomong.imongjeomongserver.util.JwtUtil;
@@ -12,21 +14,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class QuestServiceImpl implements QuestService{
+public class QuestServiceImpl implements QuestService {
 
     private final QuestRepositoy questRepositoy;
     private final MyQuestRepository myQuestRepository;
+    private final MemberRepository memberRepository;
     private final JwtUtil jwtUtil;
-
 
     @Override
     public List<MyQuestDTO> getDailyQuestList(HttpServletRequest request) {
@@ -46,17 +47,38 @@ public class QuestServiceImpl implements QuestService{
             }
 
             MyQuestDTO myQuestDTO = MyQuestDTO.builder()
-                            .id(value.getId())
-                            .name(value.getQuest().getName())
-                            .exp(value.getQuest().getExp())
-                            .gold(value.getQuest().getGold())
-                            .clearFlag(clearFlag)
-                            .rewardFlag(rewardFlag).build();
+                    .id(value.getId())
+                    .name(value.getQuest().getName())
+                    .exp(value.getQuest().getExp())
+                    .gold(value.getQuest().getGold())
+                    .clearFlag(clearFlag)
+                    .rewardFlag(rewardFlag).build();
             myQuestDTOList.add(myQuestDTO);
         });
 
         return myQuestDTOList;
 
+    }
+
+    @Transactional
+    @Override
+    public Member getQuestReward(HttpServletRequest request, Long myQuestId) {
+        String accessToken = getAccessToken(request);
+        Optional<Member> findMember = memberRepository.findById(jwtUtil.getMemberId(accessToken));
+        MyQuest findMyQuest = myQuestRepository.findById(myQuestId).get();
+        Member modifyMember = null;
+        if (findMember.isPresent()){
+            modifyMember = findMember.get();
+            Map<String, Object> paramMap = new HashMap<>();
+            paramMap.put("gold", modifyMember.getGold() + findMyQuest.getQuest().getGold());
+            paramMap.put("updateTime", LocalDateTime.now());
+            modifyMember.modifyValue(paramMap);
+            modifyMember = memberRepository.save(modifyMember);
+
+            findMyQuest.setRewardTime(LocalDateTime.now());
+            myQuestRepository.save(findMyQuest);
+        }
+        return modifyMember;
     }
 
     /* 헤더 Authorization 파싱 */
