e489534ef1781399342101535e79ed09a0953550
cksghks89 <song960705@naver.com>
2023-07-03 20:44:52 +0900

    feat: 업 업적 r  controller 일부 구현

---
+++ src/main/java/com/imongjeomong/imongjeomongserver/dto/AchievementDto.java
@@ -0,0 +1,13 @@
+package com.imongjeomong.imongjeomongserver.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AchievementDto {
+    private Long achievementId;
+}
