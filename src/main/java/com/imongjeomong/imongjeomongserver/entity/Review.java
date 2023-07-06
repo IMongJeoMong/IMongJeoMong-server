504ac05c15df37bc9ba5d3f717ebed670344965d
cksghks89 <song960705@naver.com>
2023-07-06 17:11:13 +0900

    feat: review 조회 api 구현

--- src/main/java/com/imongjeomong/imongjeomongserver/entity/Review.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/entity/Review.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -26,6 +28,9 @@ public class Review {
     @JoinColumn(name = "attraction_id")
     private Attraction attraction;
     private String content;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "review")
+    private List<ReviewImage> reviewImageList = new ArrayList<>();
 
     @Embedded
     private EditTime editTime;
