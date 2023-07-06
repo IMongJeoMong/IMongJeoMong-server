9f09f1e4f934f53875c057b65dbf82a31d75ff17
cksghks89 <song960705@naver.com>
2023-07-06 17:11:13 +0900

    feat: review 조회 api 구현

--- src/main/java/com/imongjeomong/imongjeomongserver/review/controller/ReviewController.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/review/controller/ReviewController.java
@@ -1,48 +1,37 @@
 package com.imongjeomong.imongjeomongserver.review.controller;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+
+import com.imongjeomong.imongjeomongserver.dto.ReviewDto;
 import com.imongjeomong.imongjeomongserver.exception.CommonException;
 import com.imongjeomong.imongjeomongserver.exception.CustomExceptionStatus;
+import com.imongjeomong.imongjeomongserver.response.CommonResponse;
+import com.imongjeomong.imongjeomongserver.response.DataResponse;
+import com.imongjeomong.imongjeomongserver.review.model.service.ReviewService;
 import com.imongjeomong.imongjeomongserver.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class ReviewController {
 
+    private final ReviewService reviewService;
     private final JwtUtil jwtUtil;
 
-    private final AmazonS3 amazonS3;
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-    @PostMapping("/aws/saveFile")
-    public String saveFile(@RequestParam("file") MultipartFile file) throws IOException {
-        String originalFilename = file.getOriginalFilename();
-
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-        metadata.setContentType(file.getContentType());
-
-        amazonS3.putObject(bucket, originalFilename, file.getInputStream(), metadata);
-        return amazonS3.getUrl(bucket, originalFilename).toString();
-    }
-
+    /**
+     * review 작성 (C)
+     */
     @PostMapping("/review")
-    public void writeReview(HttpServletRequest request, @ModelAttribute MultipartFile image, String content) {
+    public CommonResponse writeReview(HttpServletRequest request,
+                                      @ModelAttribute MultipartFile image,
+                                      Long attractionId, String content) throws IOException {
         String accessToken = "";
         Long memberId = null;
         try {
@@ -52,6 +41,36 @@ public class ReviewController {
             throw new CommonException(CustomExceptionStatus.TOKEN_DOES_NOT_EXISTS);
         }
 
+        // 입력값을 reviewDto로 변환 후 서비스로 전달
+        ReviewDto reviewDto = ReviewDto.builder()
+                .memberId(memberId)
+                .attractionId(attractionId)
+                .content(content)
+                .build();
+
+        reviewService.saveReview(reviewDto, image);
+
+        return new CommonResponse(200, "리뷰 저장 성공");
     }
 
+    /**
+     * review 가져오기 (R)
+     */
+    @GetMapping("/review/{attractionId}")
+    private DataResponse<List<ReviewDto>> getReviewList(@PathVariable Long attractionId) {
+        List<ReviewDto> reviewDtoList = reviewService.getReviewList(attractionId);
+
+        DataResponse<List<ReviewDto>> response = new DataResponse<>(200, "리뷰 리스트 조회 성공");
+        response.setData(reviewDtoList);
+
+        return response;
+    }
+
+    /**
+     * review 수정 (U)
+     */
+
+    /**
+     * review 삭제 (D)
+     */
 }
--- src/main/java/com/imongjeomong/imongjeomongserver/review/model/service/ReviewServiceImpl.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/review/model/service/ReviewServiceImpl.java
@@ -1,11 +1,89 @@
 package com.imongjeomong.imongjeomongserver.review.model.service;
 
+import com.imongjeomong.imongjeomongserver.attraction.model.repository.AttractionRepository;
+import com.imongjeomong.imongjeomongserver.dto.ReviewDto;
+import com.imongjeomong.imongjeomongserver.entity.Attraction;
+import com.imongjeomong.imongjeomongserver.entity.Member;
+import com.imongjeomong.imongjeomongserver.entity.Review;
+import com.imongjeomong.imongjeomongserver.entity.ReviewImage;
+import com.imongjeomong.imongjeomongserver.entity.common.EditTime;
+import com.imongjeomong.imongjeomongserver.exception.CommonException;
+import com.imongjeomong.imongjeomongserver.exception.CustomExceptionStatus;
+import com.imongjeomong.imongjeomongserver.member.model.repository.MemberRepository;
+import com.imongjeomong.imongjeomongserver.review.model.repository.ReviewImageRepository;
+import com.imongjeomong.imongjeomongserver.review.model.repository.ReviewRepository;
+import com.imongjeomong.imongjeomongserver.util.AwsS3Util;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ReviewServiceImpl implements ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final ReviewImageRepository reviewImageRepository;
+    private final AttractionRepository attractionRepository;
+    private final MemberRepository memberRepository;
+
+    private final AwsS3Util awsS3Util;
+
+    @Override
+    public void saveReview(ReviewDto reviewDto, MultipartFile image) throws IOException {
+        String imageUrl = awsS3Util.s3SaveFile(image);
+
+        // db 에 저장할 데이터 조회
+        Attraction attraction = attractionRepository.findById(reviewDto.getAttractionId())
+                .orElseThrow(() -> new CommonException(CustomExceptionStatus.ATTRACTION_NOT_FOUND));
+        Member member = memberRepository.findById(reviewDto.getMemberId())
+                .orElseThrow(() -> new CommonException(CustomExceptionStatus.AUTHENTICATION_MEMBER_IS_NULL));
+
+        // Review 저장
+        Review review = new Review();
+        review.setAttraction(attraction);
+        review.setMember(member);
+        review.setContent(reviewDto.getContent());
+        EditTime editTime = new EditTime();
+        editTime.setCreateTime(LocalDateTime.now());
+        review.setEditTime(editTime);
+        reviewRepository.save(review);
+
+        // Review Image 저장
+        ReviewImage reviewImage = new ReviewImage();
+        reviewImage.setReview(review);
+        reviewImage.setImagePath(imageUrl);
+        reviewImageRepository.save(reviewImage);
+    }
+
+    @Override
+    @Transactional
+    public List<ReviewDto> getReviewList(Long attractionId) {
+        List<ReviewDto> resultList = new ArrayList<>();
+
+        List<Review> reviewList = reviewRepository.findByAttractionId(attractionId);
+
+        for (Review review : reviewList) {
+            ReviewDto reviewDto = ReviewDto.builder()
+                    .reviewId(review.getId())
+                    .memberId(review.getMember().getId())
+                    .memberName(review.getMember().getNickname())
+                    .attractionId(attractionId)
+                    .imagePath(review.getReviewImageList().get(0).getImagePath())
+                    .content(review.getContent())
+                    .createTime(review.getEditTime().getCreateTime())
+                    .build();
+            resultList.add(reviewDto);
+        }
+
+        return resultList;
+    }
+
 }
