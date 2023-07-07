358b89c7a073e3c485f67ae8fe6b3fe6a77415cb
Junyoung <wnsdud12365@naver.com>
2023-07-07 13:35:05 +0900

    feat: 관광지 방문 처리 시 거리 기반으로 성공 여부 추가

--- src/main/java/com/imongjeomong/imongjeomongserver/attraction/model/repository/AttractionRepository.java
+++ src/main/java/com/imongjeomong/imongjeomongserver/attraction/model/repository/AttractionRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AttractionRepository extends JpaRepository<Attraction,Long> {
 
@@ -25,4 +27,15 @@ public interface AttractionRepository extends JpaRepository<Attraction,Long> {
                                                   @Param("lat") Double lat,
                                                   @Param("lng") Double lng,
                                                   Pageable pageable);
+
+    @Query(value = "SELECT new com.imongjeomong.imongjeomongserver.dto.AttractionDTO(" +
+            "a.id, a.name, a.address, a.lat, a.lng, a.description, a.contentTypeId, a.imagePath, " +
+            "a.tel, a.sidoCode, a.gold, a.exp, " +
+            "(6371 * 2 * ASIN(SQRT(POWER(SIN((:lat - a.lat) * pi()/180 / 2), 2) + " +
+            "COS(:lat * pi()/180) * COS(a.lat * pi()/180) * POWER(SIN((:lng - a.lng) * pi()/180 / 2), 2)))) * 1000 AS distance )" +
+            " FROM Attraction a " +
+            "WHERE a.id = :attractionId " +
+            "ORDER BY distance",
+            countQuery = "SELECT COUNT(*) From Attraction a")
+    Optional<AttractionDTO> findByAttractionId(@Param("attractionId") Long attractionId, @Param("lat") Double lat, @Param("lng") Double lng);
 }
